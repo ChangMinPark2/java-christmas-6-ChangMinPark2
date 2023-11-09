59638e0ab4dc373f3d34c12e77ab4bdf5a43f24b
CHANG MIN PARK <98qkrckdals@naver.com>
2023-11-09 18:10:35 +0900

    feat(Christmas Promotion): 메뉴 카테고리에 따른 메뉴 구현 카테고리 별로 다른 메뉴를 가진 메뉴판을 구현하기 위해서 메뉴 카테고리와, 메뉴를 enum으로 구현했습니다. menuCategory는 애피타이저, 메인, 디저트, 음료만 가지고 있습니다. menu는 카테고리에 맞는 음식 메뉴와, 가격을 정의하였습니다.

---
+++ src/main/java/christmas/domain/Menu.java
@@ -0,0 +1,38 @@
+package christmas.domain;
+
+public enum Menu {
+    MUSHROOM_SOUP("양송이수프", MenuCategory.APPETIZER, 6000),
+    TAPAS("타파스", MenuCategory.APPETIZER, 5500),
+    CAESAR_SALAD("시저셀러드", MenuCategory.APPETIZER, 8000),
+    T_BONE_STEAK("티본스테이크", MenuCategory.MAIN, 55000),
+    BBQ_RIB("바베큐립", MenuCategory.MAIN, 54000),
+    SEAFOOD_PASTA("해산물파스타", MenuCategory.MAIN, 35000),
+    CHRISTMAS_PASTA("크리스마스파스타", MenuCategory.MAIN, 25000),
+    CHOCO_CAKE("초코케이크", MenuCategory.DESSERT, 15000),
+    ICE_CREAM("아이스크림", MenuCategory.DESSERT, 5000),
+    ZERO_COLA("제로콜라", MenuCategory.DRINK, 3000),
+    RED_WINE("레드와인", MenuCategory.DRINK, 60000),
+    CHAMPAGNE("샴페인", MenuCategory.DRINK, 25000);
+
+    private final String name;
+    private final MenuCategory category;
+    private final int price;
+
+    Menu(String name, MenuCategory category, int price) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public MenuCategory getCategory() {
+        return category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
