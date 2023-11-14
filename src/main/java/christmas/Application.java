7aa4bbd7ab913c721c2adb4ed8723d20e4112fe0
CHANG MIN PARK <98qkrckdals@naver.com>
2023-11-15 02:30:07 +0900

    style(Christmas Promotion): 공백제거

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -3,7 +3,6 @@ package christmas;
 import christmas.controller.ChristmasPromotionController;
 
 public class Application {
-
     public static void main(String[] args) {
         ChristmasPromotionController christmasPromotionController = new ChristmasPromotionController();
         christmasPromotionController.start();
