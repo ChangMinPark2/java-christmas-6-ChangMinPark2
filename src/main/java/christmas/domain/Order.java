bc2f833c3ca2fa0df215d1f44733f3acf311d46c
CHANG MIN PARK <98qkrckdals@naver.com>
2023-11-14 18:48:29 +0900

    refactor(Christmas Promotion): 메뉴 생성 구현, 예외 추가 올바른 값이 들어오는지에 대한 예외 메서드들을 추가해주었습니다. 사용자의 입력 값을 통해 메뉴를 생성하는 기능을 추가 주었습니다.

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -4,30 +4,41 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.stream.Collectors;
 
+import static christmas.util.GlobalConstant.*;
+import static christmas.util.Validator.*;
+
 public class Order {
     private final int visitDay;
     private final HashMap<Menu, Integer> orderMenu;
 
-    public Order(int visitDay, String inputMenuAndCount) {
-        validateNotRange(visitDay);
-        this.visitDay = visitDay;
+    public Order(String visitDay, String inputMenuAndCount) {
+        int day = validateIsNumber(visitDay);
+        validateNotEmptyMenu(inputMenuAndCount);
+        validateHasMenu(inputMenuAndCount);
+        validateCount(inputMenuAndCount);
+        validateNotExceedMax(inputMenuAndCount);
+        validateNoDuplicateMenu(inputMenuAndCount);
+        validateNotOnlyDrinks(inputMenuAndCount);
+        this.visitDay = validateDayInRange(day);
         this.orderMenu = createMenu(inputMenuAndCount);
     }
 
+    public int getVisitDay() {
+        return visitDay;
+    }
+
+    public HashMap<Menu, Integer> getOrderMenu() {
+        return orderMenu;
+    }
+
     private HashMap<Menu, Integer> createMenu(String inputMenuAndCount) {
-        return Arrays.stream(inputMenuAndCount.split(","))
-                .map(menuAndCount -> menuAndCount.split("-"))
+        return Arrays.stream(inputMenuAndCount.split(COMMA))
+                .map(menuAndCount -> menuAndCount.split(MINUS))
                 .collect(Collectors.toMap(
-                        menuAndCountArr -> Menu.fromName(menuAndCountArr[0]),
-                        menuAndCountArr -> Integer.parseInt(menuAndCountArr[1]),
+                        menuAndCountArr -> Menu.fromName(menuAndCountArr[MENU_NAME]),
+                        menuAndCountArr -> Integer.parseInt(menuAndCountArr[MENU_COUNT]),
                         (oldValue, newValue) -> oldValue,
                         HashMap::new
                 ));
-    }
-
-    private void validateNotRange(int visitDay) {
-        if (visitDay < 1 || visitDay > 31) {
-            throw new IllegalArgumentException();
-        }
     }
 }
