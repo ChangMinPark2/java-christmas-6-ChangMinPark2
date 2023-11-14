23e6c447d40c05e828ceaf98b6e8441fcc052f11
CHANG MIN PARK <98qkrckdals@naver.com>
2023-11-14 18:32:51 +0900

    refactor(Christmas Promotion): 상수 모음 클래스 선언 GlobalConstant에서 각 로직에 필요한 매직넘버를 모두 선언해 줌으로써, InputView에서는 입력 값만 받는 역할을 해주었습니다.

---
+++ src/main/java/christmas/util/GlobalConstant.java
@@ -0,0 +1,11 @@
+package christmas.util;
+
+public final class GlobalConstant {
+    public static final String COMMA = ",";
+    public static final String MINUS = "-";
+    public static final int MIN_DAY = 1;
+    public static final int MAX_DAY = 31;
+    public static final int ORDER_MAX_COUNT = 20;
+    public static final int MENU_NAME = 0;
+    public static final int MENU_COUNT = 1;
+}
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,9 +2,9 @@ package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class InputView {
-    public static final String COMMA = ",";
-    public static final String CONDITION = "-";
+public final class InputView {
+    private InputView() {
+    }
 
     public static String getExpectedVisitDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
