944629dfe7942203cbba099ccf0e6697e009a3c4
CHANG MIN PARK <98qkrckdals@naver.com>
2023-11-14 18:28:52 +0900

    refactor(Christmas Promotion): 상수추가 12월 이벤트 배지 총 3개에 대하여 기준 금액을 추가해주었습니다.

--- src/main/java/christmas/domain/DecemberEventBadge.java
+++ src/main/java/christmas/domain/DecemberEventBadge.java
@@ -6,6 +6,10 @@ public enum DecemberEventBadge {
     TREE("트리"),
     NO_BADGE("없음");
 
+    public static final int STAR_MIN_AMOUNT = 5000;
+    public static final int TREE_MIN_AMOUNT = 10000;
+    public static final int SANTA_MIN_AMOUNT = 20000;
+
     private final String badgeName;
 
     DecemberEventBadge(String badgeName) {
