ec54d3547cc209ec7e6e42665a192bb72d20dce2
CHANG MIN PARK <98qkrckdals@naver.com>
2023-11-14 18:55:00 +0900

    refactor(Christmas Promotion): 가독성, 생성자 private, 클래스 명 수정 OutPutView는 출력을 담당하는 UI이므로, 생성할 수 없어 private로 생성자를 선언해 주었습니다. 필요 없는 코드를 삭제해 주었습니다. 클래스 명을 OutputView로 수정해 주었습니다.

--- src/main/java/christmas/view/OutPutVIew.java
+++ src/main/java/christmas/view/OutPutVIew.java
@@ -5,79 +5,91 @@ import christmas.domain.OrderDetails;
 
 import java.text.NumberFormat;
 
-public class OutPutVIew {
-    public final String GAME_GUID = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+import static christmas.util.GlobalConstant.MINUS;
 
-    public void printEventPreview(String visitDay) {
+public final class OutputView {
+    public static final String GAME_GUID = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+
+    private OutputView() {
+    }
+
+    public static void printEventPreview(String visitDay) {
         System.out.println("12월 " + visitDay + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n");
     }
 
-    public void printOrderedMenuList(OrderDetails orderDetails) {
+    public static void printOrderedMenuList(OrderDetails orderDetails) {
         System.out.println("<주문 메뉴>");
 
         for (Menu menu : orderDetails.getOrderedMenus().keySet()) {
             System.out.println(menu.getName() + " " + orderDetails.getOrderedMenus().get(menu) + "개");
         }
+
         System.out.println();
     }
 
-    public void printTotalOrderAmountBeforeDiscount(OrderDetails orderDetails) {
+    public static void printTotalOrderAmountBeforeDiscount(OrderDetails orderDetails) {
         NumberFormat formatter = NumberFormat.getInstance();
 
         System.out.println("<할인 전 총주문 금액>");
-        System.out.println(formatter.format(orderDetails.getTotalOrderAmountBeforeDiscount()) + "원");
-        System.out.println();
+        System.out.printf("%s원\n\n", formatter.format(orderDetails.getTotalOrderAmountBeforeDiscount()));
     }
 
-    public void printGiftMenu(OrderDetails orderDetails) {
+    public static void printGiftMenu(OrderDetails orderDetails) {
         System.out.println("<증정 메뉴>");
 
         if (!orderDetails.isGiftMenuIncluded()) {
-            System.out.println("없음");
-            System.out.println();
+            System.out.println("없음\n");
             return;
         }
-        System.out.println("샴페인 1개");
-        System.out.println();
+
+        System.out.println("샴페인 1개\n");
     }
 
-    public void printBenefitList(OrderDetails orderDetails) {
+    public static void printBenefitList(OrderDetails orderDetails) {
         System.out.println("<혜택 내역>");
 
-        NumberFormat formatter = NumberFormat.getInstance();
         if (!orderDetails.getBenefitsReceived().isEmpty()) {
-            for (String name : orderDetails.getBenefitsReceived().keySet()) {
-                System.out.println(name + ": -" + formatter.format(orderDetails.getBenefitsReceived().get(name)) + "원");
-            }
-            System.out.println();
+            printBenefits(orderDetails);
             return;
         }
-        System.out.println("없음");
+
+        System.out.println("없음\n");
     }
 
-    public void printTotalBenefitAmount(OrderDetails orderDetails) {
+    public static void printTotalBenefitAmount(OrderDetails orderDetails) {
         System.out.println("<총혜택 금액>");
-
         NumberFormat formatter = NumberFormat.getInstance();
+
         if (orderDetails.getTotalBenefitValue() != 0) {
-            System.out.println("-" + formatter.format(orderDetails.getTotalBenefitValue()) + "원");
-            System.out.println();
+            System.out.printf("%s%s원\n\n", MINUS, formatter.format(orderDetails.getTotalBenefitValue()));
             return;
         }
-        System.out.println("0원");
-        System.out.println();
+
+        System.out.println("0원\n");
     }
 
-    public void printEstimatedPaymentAfterDiscount(OrderDetails orderDetails) {
+    public static void printEstimatedPaymentAfterDiscount(OrderDetails orderDetails) {
         System.out.println("<할인 후 예상 결제 금액>");
-
         NumberFormat formatter = NumberFormat.getInstance();
-        System.out.println(formatter.format(orderDetails.getEstimatedPaymentAfterDiscount()) + "원");
-        System.out.println();
+        System.out.printf("%s원\n\n", formatter.format(orderDetails.getEstimatedPaymentAfterDiscount()));
     }
 
-    public void printDecemberEventBadge(OrderDetails orderDetails) {
+    public static void printDecemberEventBadge(OrderDetails orderDetails) {
         System.out.println("<12월 이벤트 배지>");
         System.out.println(orderDetails.getDecemberEventBadge().getBadgeName());
+    }
+
+    private static void printBenefits(OrderDetails orderDetails) {
+        NumberFormat formatter = NumberFormat.getInstance();
+        for (String name : orderDetails.getBenefitsReceived().keySet()) {
+            System.out.printf(
+                    "%s: %s%s원\n",
+                    name,
+                    MINUS,
+                    formatter.format(orderDetails.getBenefitsReceived().get(name))
+            );
+        }
+
+        System.out.println();
     }
 }
